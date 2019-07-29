ac2af786f612d2c1016de82831205befc7da9bce
Jake <jake@Marissas-MacBook-Pro-2.local>
2019-07-29 17:55:48 -0600

    Updates

---
+++ precache-manifest.44d497a6d1873c4c1f07bddc44b24cd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2a66d5eb8040df817599d15ba19618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee5b88ee4f92525a5b41",
+    "url": "/static/css/main.565f3bb6.chunk.css"
+  },
+  {
+    "revision": "308b8d54220431a40ebd",
+    "url": "/static/js/2.ddc85e99.chunk.js"
+  },
+  {
+    "revision": "ee5b88ee4f92525a5b41",
+    "url": "/static/js/main.be3cd9f3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
