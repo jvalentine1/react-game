6c4e8bbf428bed6d047a1bc0b0195f5d2fbe7ab0
Jake <jake@Marissas-MacBook-Pro-2.local>
2019-07-29 17:58:38 -0600

    Updates

---
+++ precache-manifest.e448958ad7a42406dc0df27ea3d0a729.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34723dcbe5d7339d0ffcac2c4f03b9f",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "b402e65f71cc64e5b518",
+    "url": "/react-game/static/css/main.565f3bb6.chunk.css"
+  },
+  {
+    "revision": "308b8d54220431a40ebd",
+    "url": "/react-game/static/js/2.ddc85e99.chunk.js"
+  },
+  {
+    "revision": "b402e65f71cc64e5b518",
+    "url": "/react-game/static/js/main.284dfad2.chunk.js"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  }
+]);
